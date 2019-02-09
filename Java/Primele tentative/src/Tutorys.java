6c82759178b95c3b255435b4b827b330a7c6ed82
Cristina <36567051+CristinaRichter1@users.noreply.github.com>
2019-02-09 13:44:18 +0200

    Add files via upload

---
+++ Java/Primele tentative/src/Tutorys.java
@@ -0,0 +1,13 @@
+
+public class Tutorys {
+	public static void main (String args[]){
+	System.out.println("This is  my first java command!");
+	}
+}
+
+
+//fil -> new -> java project=> project name : test-project => next => finish
+//fisierul de src
+
+//pachet - fiecare clasa trebuie sa aiba tab?? diferit
+//pentru a face o mai buna diferentiere intre ele, iar fisierele sa aiba un singur scop.
---
+++ Java/Primele tentative/src/ro/ase/java/HelloJava.java
@@ -0,0 +1,8 @@
+package ro.ase.java;
+
+public class HelloJava {
+
+	public static void main (String[] args) {
+		System.out.println("HelloJava");
+	}
+}
